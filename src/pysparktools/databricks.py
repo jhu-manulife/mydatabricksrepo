10d6ff232f1b5eb7acc77b058e079e559900aa1d
jhu-manulife <jingyi_hu@manulife.com>
2022-12-30 10:53:51 -0500

    first commit

---
+++ src/pysparktools/databricks.py
@@ -0,0 +1,16 @@
+'''
+For interaction with Databricks Delta Lake
+'''
+
+class deltaTable(object):
+    def __init__(self) -> None:
+        pass
+
+    def __str__(self) -> str:
+        return "For interaction with Databricks Delta Table"
+
+    def IAmHere(self):
+        '''
+        Tell them I am here
+        '''
+        return "I am here"
